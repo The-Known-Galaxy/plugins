7fa571afa16df1c8d482d04bdbd6119d821aa6f1
ShadowEngineer <ShadowEngineer54@gmail.com>
2024-03-28 17:35:53 +0000

    add unit test runner

---
+++ .commands.sh
@@ -0,0 +1,10 @@
+run_unit_tests() {
+    echo "TOOL VERSIONS"
+    rojo --version
+    rit --version
+    echo "BUILDING PLACE"
+    rit build -p default
+    echo "RUNNING UNIT TESTS"
+    run-in-roblox --place build/default.rbxl --script run-unit-tests.luau
+    echo "TEST RUNNING FINISHED"
+}

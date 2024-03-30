0a635fb282c7290ae77af4df33462425792be249
ShadowEngineer <ShadowEngineer54@gmail.com>
2024-03-30 15:51:43 +0000

    implement unit test runner into lune

--- .commands.sh
+++ .commands.sh
@@ -1,14 +1,3 @@
-run_unit_tests() {
-    echo "TOOL VERSIONS"
-    rojo --version
-    rit --version
-    echo "BUILDING PLACE"
-    rit build -p default
-    echo "RUNNING UNIT TESTS"
-    run-in-roblox --place build/default.rbxl --script run-unit-tests.luau
-    echo "TEST RUNNING FINISHED"
-}
-
 run_static_analysis() {
     echo "TOOL VERSIONS"
     selene --version

a5118a1a08405c2f12b74d2a4a65afdee65915d6
ShadowEngineer <ShadowEngineer54@gmail.com>
2024-03-30 21:00:41 +0000

    setup moonwave for publishin

--- .commands.sh
+++ .commands.sh
@@ -7,6 +7,15 @@ docsite() {
     echo "DOC SITE STOPPED"
 }
 
+docsuite_build() {
+    echo "TOOL VERSIONS"
+    echo "npm $(npm --version)"
+    echo "moonwave $(./node_modules/.bin/moonwave --version)"
+    echo "BUILDING DOC SITE AND PUBLISHING"
+    ./node_modules/.bin/moonwave build --publish --out-dir moonwave_build
+    echo "DOC SITE BUILT"
+}
+
 plugins() {
     lune --version
     lune run main

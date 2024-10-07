6584bc1200b493e9990f0bc494f798beff83377a
ShadowEngineer <ShadowEngineer54@gmail.com>
2024-10-07 19:30:57 +0100

    Built and published by Moonwave

---
+++ assets/js/8be7253f.d4e78f10.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4176],{49904:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"ArchiveValidator","desc":"Validates the contents of the archives when they are selected.","source":{"line":7,"path":"src/PluginModules/ArchiveValidator.module/init.luau"}}')}}]);
